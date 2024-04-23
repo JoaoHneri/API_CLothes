d39a0537a0b5dd08a576b64b7b9c1498e14c497b
João Henrique <joaohnsousa@gmail.com>
2024-04-23 15:49:44 -0300

    Estatic

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const cors = require('cors');const app = express();
+const path = require("path");
 require('dotenv').config()
 
 app.use(express.json());
+
+app.use('/uploads', express.static(path.join(__dirname, '../uploads')))
+
 app.use(cors());
 const conn = require("./db/conn");
 const router= require("./routes/router")
