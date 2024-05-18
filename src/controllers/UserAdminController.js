e32302f0fd28cf89041a2e976720d3ca22f2f728
João Henrique <joaohnsousa@gmail.com>
2024-05-18 19:31:36 -0300

    UserAdmin get

--- src/controllers/UserAdminController.js
+++ src/controllers/UserAdminController.js
@@ -1,64 +1,69 @@
-const userAdmin = require('../models/UserAdmin');
+const userAdmin = require("../models/UserAdmin");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const { promisify } = require("util");
 const verifyJwt = promisify(jwt.verify);
+const  userBuy = require("../models/User");
+const paymentModel = require("../models/PaymentModel");
 
 
 const registerAdminUser = async (req, res) => {
-    const { userName, userEmail, userPassword, isAdmin } = req.body;
-  
-    if (!userEmail) {
-      return res.status(422).send({ msg: "O campo email deve ser preenchido" });
-    }
-    if (!userPassword) {
-      return res.status(422).send({ msg: "O campo password deve ser preenchido" });
-    }
-    if (!userName) {
-      return res.status(422).send({ msg: "O campo Nome deve ser preenchido" });
-    }
-    if (isAdmin === undefined) {
-      return res.status(422).send({ msg: "O campo isAdmin deve ser preenchido" });
-    }
-  
-    const userExist = await userAdmin.findOne({ userEmail: userEmail });
-  
-    if (userExist) {
-      return res.status(422).send({
-        msg: "Já existe um usuário cadastrado com esse email, por favor tente com outro",
-      });
-    }
-  
-    // Introdução ao bcrypt
-    const salt = await bcrypt.genSalt(12);
-    const passwordHash = await bcrypt.hash(userPassword, salt);
-  
-    try {
-      const user = await userAdmin.create({
-        userName,
-        userEmail,
-        userPassword: passwordHash,
-        isAdmin,
-        Rent: 0,  // Definindo Rent como 0
-        Orders: 0 // Definindo Orders como 0
-      });
-  
-      // Gerar token JWT incluindo o ID do usuário no payload
-      const secret = process.env.SECRET;
-      const token = jwt.sign(
-        {
-          id: user._id.toString(), // Incluir o ID do novo usuário no payload
-        },
-        secret
-      );
-  
-      await enviarEmail(userEmail, userName);
-      return res.status(200).json({ msg: "Usuário registrado com sucesso", token, userId: user._id }); // Inclui o ID do usuário na resposta
-    } catch (error) {
-      return res.status(400).json(error);
-    }
-  };
+  const { userName, userEmail, userPassword, isAdmin } = req.body;
 
+  if (!userEmail) {
+    return res.status(422).send({ msg: "O campo email deve ser preenchido" });
+  }
+  if (!userPassword) {
+    return res
+      .status(422)
+      .send({ msg: "O campo password deve ser preenchido" });
+  }
+  if (!userName) {
+    return res.status(422).send({ msg: "O campo Nome deve ser preenchido" });
+  }
+  if (isAdmin === undefined) {
+    return res.status(422).send({ msg: "O campo isAdmin deve ser preenchido" });
+  }
+
+  const userExist = await userAdmin.findOne({ userEmail: userEmail });
+
+  if (userExist) {
+    return res.status(422).send({
+      msg: "Já existe um usuário cadastrado com esse email, por favor tente com outro",
+    });
+  }
+
+  // Introdução ao bcrypt
+  const salt = await bcrypt.genSalt(12);
+  const passwordHash = await bcrypt.hash(userPassword, salt);
+
+  try {
+    const user = await userAdmin.create({
+      userName,
+      userEmail,
+      userPassword: passwordHash,
+      isAdmin,
+      Rent: 0, // Definindo Rent como 0
+      Orders: 0, // Definindo Orders como 0
+    });
+
+    // Gerar token JWT incluindo o ID do usuário no payload
+    const secret = process.env.SECRET;
+    const token = jwt.sign(
+      {
+        id: user._id.toString(), // Incluir o ID do novo usuário no payload
+      },
+      secret
+    );
+
+    await enviarEmail(userEmail, userName);
+    return res
+      .status(200)
+      .json({ msg: "Usuário registrado com sucesso", token, userId: user._id }); // Inclui o ID do usuário na resposta
+  } catch (error) {
+    return res.status(400).json(error);
+  }
+};
 
 const loginAdminUser = async (req, res) => {
   const { userEmail, userPassword } = req.body;
@@ -67,7 +72,9 @@ const loginAdminUser = async (req, res) => {
     return res.status(422).send({ msg: "O campo email deve ser preenchido" });
   }
   if (!userPassword) {
-    return res.status(422).send({ msg: "O campo password deve ser preenchido" });
+    return res
+      .status(422)
+      .send({ msg: "O campo password deve ser preenchido" });
   }
 
   try {
@@ -76,7 +83,10 @@ const loginAdminUser = async (req, res) => {
       return res.status(404).send({ msg: "Usuário não encontrado" });
     }
 
-    const isPasswordValid = await bcrypt.compare(userPassword, user.userPassword);
+    const isPasswordValid = await bcrypt.compare(
+      userPassword,
+      user.userPassword
+    );
     if (!isPasswordValid) {
       return res.status(401).send({ msg: "Senha inválida" });
     }
@@ -90,7 +100,14 @@ const loginAdminUser = async (req, res) => {
       secret
     );
 
-    return res.status(200).json({ msg: "Login realizado com sucesso", token, userId: user._id, isAdmin: true });
+    return res
+      .status(200)
+      .json({
+        msg: "Login realizado com sucesso",
+        token,
+        userId: user._id,
+        isAdmin: true,
+      });
   } catch (error) {
     return res.status(500).json({ msg: "Erro interno do servidor", error });
   }
@@ -99,23 +116,30 @@ const loginAdminUser = async (req, res) => {
 const getAdminUser = async (req, res) => {
   const { user_id } = req.params;
 
-  if(!userId) {
-    res.status(401).send({ msg: " Inserir userId" });
-
-    try {
-      const user = await userAdmin.findById(user_id);
-      if(user) {
-        return res.status(200).json(user);
-      }
-    } catch (error) {
-      return res.status(500).json({ msg: "Erro interno ao encontrar usuario", error });
-    }
-}
-}
-
-  
-  module.exports = {
-    registerAdminUser,
-    loginAdminUser,
-    getAdminUser
+  if (!user_id) {
+    return res.status(401).send({ msg: "Inserir user_id" });
   }
+
+  try {
+    const usersLength = await userBuy.countDocuments();
+    const totalOrders = await paymentModel.countDocuments();
+    const user = await userAdmin.findById(user_id);
+    if (user) {
+      const { userName, isAdmin, Rent, Orders } = user;
+      return res.status(200).json({ userName, isAdmin, Rent, Orders, usersLength, totalOrders });
+    } else {
+      return res.status(404).json({ msg: "Usuário não encontrado" });
+    }
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ msg: "Erro interno ao encontrar usuario", error });
+  }
+};
+
+
+module.exports = {
+  registerAdminUser,
+  loginAdminUser,
+  getAdminUser,
+};
